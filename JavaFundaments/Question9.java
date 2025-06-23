9d3b40e2855c8edd90bf17242b261f3acfe1e876
YUKTA VARSHNEY <125341433+yukta08@users.noreply.github.com>
2025-06-23 13:18:41 +0530

    Delete JavaFundaments directory

--- JavaFundaments/Question9.java
+++
@@ -1,9 +0,0 @@
-//Create a program to convert the distance of 10.8 kilometers to miles
-public class Question9 {
-    public static void main(String[] args) {
-      double distance =10.8;
-      double miles = distance /1.6;
-      System.out.print(" The distance"+" "+ distance +"km" +" "+" in miles is"+" "+miles);
-
-    }
-}
