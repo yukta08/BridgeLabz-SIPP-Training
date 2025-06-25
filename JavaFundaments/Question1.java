76b65556f18fd4bc5555c35dcf2cdbeb410db86e
Yukta Varshney <yuktavarshney18@gmail.com>
2025-06-25 12:14:59 +0530

    [yukta].

--- JavaFundaments/Question1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Question1{ 
-    public static void main(String[] args) {
-        int by = 2000;
-        int cy = 2024;
-        int age = cy - by;
-        System.out.println("Harry's age in " + cy + " is" + " " + age);
-        }
-}
